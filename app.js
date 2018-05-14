7b3f578a5ab180f192162e68190edfe1e4c4716f
Sommai Krangpanich <sommai.k@gmail.com>
2018-05-14 11:32:45 +0700

    add module pattern 4

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ foo2("v1","v2", "v3");
 
 var bar2 = require("./bar");
 bar2();
+
+var fz = require('./fiz').fiz; 
+fz();
+
+var fz2 = require('./fiz').fiz2;
+fz2('xxxxx');
+
+var fizz = require('./fiz');
+fizz.fiz();
+fizz.fiz2("yyyy");
+
+var buz = require('./buz'); 
+buz.log();
+buz.log2('John');
+
+var baz = require('./baz').Baz;
+baz.log();
---
+++ fiz.js
@@ -0,0 +1,7 @@
+exports.fiz = function(){
+    console.log("Fiz!");
+}
+
+exports.fiz2 = function(msg){
+    console.log("Fiz! "+msg);
+}
