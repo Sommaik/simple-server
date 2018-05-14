a80d706877b8a5a00df5e9086af3048b35096322
Sommai Krangpanich <sommai.k@gmail.com>
2018-05-14 11:06:25 +0700

    add module pattern 1

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var http = require('http');
+// var http = require('http');
 
-http.createServer(function(req,res){ 
-	res.writeHead(200,{'Content-Type':'text/plain'});
-	res.end('yes you can\n');
-}).listen(8000);
+// http.createServer(function(req,res){ 
+// 	res.writeHead(200,{'Content-Type':'text/plain'});
+// 	res.end('yes you can\n');
+// }).listen(8000);
 
-console.log("server running port 8000"); 
+// console.log("server running port 8000"); 
+
+require("./foo");
+foo();
+foo2("v1","v2", "v3");
